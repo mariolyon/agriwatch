d17b3d93f130c24b9b78d26db2c1df5c8de621a4
Mario Lyon <mario@digileo.com>
2026-04-10 19:23:27 +0200

    add column to database for temperature scale

---
+++ drizzle/migrations/0002_first_polaris.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "users" ADD COLUMN "scale" text DEFAULT 'C';
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,9 +1,10 @@
-import { pgTable, serial, jsonb, uuid } from 'drizzle-orm/pg-core'
+import { pgTable, serial, jsonb, uuid, text } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
 	userId: uuid('user_id').notNull(),
 	data: jsonb('data').notNull().default('[]'),
+	scale: text('scale').default('C'),
 })
 
 export const userLocations = users
