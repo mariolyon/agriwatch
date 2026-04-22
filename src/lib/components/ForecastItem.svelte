5fc80517963751b20266e22bc5dbb12f9d1724a8
Mario Lyon <mario@digileo.com>
2026-04-22 23:16:32 +0200

    fix the width of the ForecastItem component

--- src/lib/components/ForecastItem.svelte
+++ src/lib/components/ForecastItem.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div
-	class="flex min-w-[5ch] box-content flex-col items-center justify-between rounded-lg border border-gray-200 bg-gray-50 p-3 text-center shadow-sm"
+	class="flex w-[11ch] shrink-0 flex-col items-center justify-between rounded-lg border border-gray-200 bg-gray-50 p-3 text-center shadow-sm"
 >
 	<div class="mb-1 text-sm font-medium text-gray-500">{date}</div>
 	<div class="text-lg font-semibold text-gray-800">
